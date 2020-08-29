0f6fcf63aacf099e85b285acdf37e5e641e597c7
Anton <nirvs@mail.ru>
2020-08-30 00:23:45 +0300

    finish table logic

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -4,16 +4,38 @@ export class ExcelComponent extends DomListener {
   constructor($root, options = {}) {
     super($root, options.listeners)
     this.name = options.name || ''
+    this.emitter = options.emitter
+    this.unsubscibers = []
+    this.prepare()
   }
+  // Настраиваем наш компонент до init
+  prepare() {
+
+  }
+
   // Возвращает шаблон компонента
   toHTML() {
     return ''
   }
 
+  // Уведомляем слушателей про событие евент
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  // Подписываемся на событие евент
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscibers.push(unsub)
+  }
+  // Инициализируем компонент
   init() {
     this.initDOMListeners()
   }
+
+  // Удаляем компонент
   destroy() {
     this.removeDOMListeners()
+    this.unsubscibers.forEach(unsub => unsub())
   }
 }
